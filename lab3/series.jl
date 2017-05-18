58d957da13eda284e386148dde1390fb8fea3348
Paweł Matejko <matejko.pawel@gmail.com>
2017-05-18 10:45:36 +0200

    Using given symbol instead of changing it

--- lab3/series.jl
+++ lab3/series.jl
@@ -1,96 +1,58 @@
 
 
-macro changeSymbolValue(sym, val)
-  return esc(:($sym = $val))
-end
-
 macro fill_series(ex)
-  indexes = Array{Int,1}()
 
   arr = eval(ex.args[1].args[1])
+
+  if typeof(ex.args[1].args[2]) == Symbol
+    sym = ex.args[1].args[2]
+  else
+    sym = ex.args[1].args[2].args[2]
+  end
+
+  indexes = Array{Int,1}()
   findIndexes(ex.args, indexes)
 
   max, min = maxAndMin(indexes)
   diff = max - min
   fst = diff - indexes[1] + 1
   last = length(arr) - indexes[1]
+  range = fst:last
 
-  global sym = 0
-  ex.args = changeSymbols(ex.args, :(sym))
-
-  for i = fst:last
-    @changeSymbolValue sym i
-    eval(ex)
+  quote
+    for $sym in $range
+      $ex
+    end
   end
 
-  return arr
 end
 
 
 
 
-function findIndexes(ex::Array{Any,1}, ind)
+function findIndexes(ex::Array{Any,1}, ind::Array{Int,1})
   for i = 1:length(ex)
     findIndexes(ex[i], ind)
   end
 end
 
-function findIndexes(ex::Expr, ind)
+function findIndexes(ex::Expr, ind::Array{Int,1})
   if ex.head == :call
-    findIndexes(ex.args, ind)
+    findIndexes(ex.args[2:end], ind)
   elseif ex.head == :ref
     findOneIndex(ex.args[2], ind)
   end
 end
 
-function findIndexes(ex::Symbol, ind)
-end
-
-function findOneIndex(ex::Symbol, ind)
+function findOneIndex(ex::Symbol, ind::Array{Int,1})
   push!(ind, 0)
 end
 
-function findOneIndex(ex::Expr, ind)
+function findOneIndex(ex::Expr, ind::Array{Int,1})
   index = eval(Expr(:call, ex.args[1], ex.args[3]))
   push!(ind, index)
 end
 
-function changeSymbols(ex::Array{Any,1}, ind::Symbol)
-  for i = 1:length(ex)
-    ex[i] = changeSymbols(ex[i], ind)
-  end
-  ex
-end
-
-
-
-
-
-
-function changeSymbols(ex::Expr, ind::Symbol)
-  if ex.head == :call
-    for i = 1:length(ex.args)
-      ex.args[i] = changeSymbols(ex.args[i], ind)
-    end
-  elseif ex.head == :ref
-    ex.args[2] = changeOneSymbol(ex.args[2], ind)
-  end
-
-  ex
-end
-
-function changeSymbols(ex::Symbol, ind::Symbol)
-  ex
-end
-
-function changeOneSymbol(ex::Symbol, ind::Symbol)
-  ex = :($ind)
-end
-
-function changeOneSymbol(ex::Expr, ind::Symbol)
-  ex.args[2] = :($ind)
-  ex
-end
 
 
 
